568d5d57bd0d40e78dc9b2bfb81c77e26f6af0d9
Vaibhav Solanki <vaibhavsolanki026@gmail.com>
2024-08-16 16:13:57 +0530

    first commit

---
+++ frontend/src/components/LoadingIndecator.jsx
@@ -0,0 +1,8 @@
+import '../styles/LoadingIndicator.css'
+
+const LoadingIndecator = () => {
+    return <div className='loading-container'>
+        <div className='loader'></div>
+    </div>
+}
+export default LoadingIndecator
